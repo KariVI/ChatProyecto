d9481cb2884d1704e6d3597194b83db3b7544090
KariVI <47987287+KariVI@users.noreply.github.com>
2021-09-28 23:49:46 -0500

    se añadio interfaz

---
+++ ChatProyecto/IChatService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatProyecto
+{
+   [ServiceContract]
+    public interface IChatService
+    {
+      
+        
+        [OperationContract]
+        Message GetMessage();
+        
+
+
+    }
+
+    [DataContract]
+    public class Message {
+        private String userName;
+        private String text;
+
+        [DataMember]
+        public String UserName { get { return userName; } set { userName = value; } }
+        [DataMember]
+        public String Text { get { return text; } set { text = value; } }
+    
+    }
+}
